82ecbd10d8199b0773fc709a63d008b680484a08
Drakime <42014144+Drakime@users.noreply.github.com>
2023-07-19 21:48:25 +0100

    Add function to play one round of game
    Note that the return values of the
    getComputerChoice() function was
    converted to lowercase for consistency.

--- script.js
+++ script.js
@@ -2,15 +2,48 @@ function getComputerChoice() {
   let randomNumber = Math.floor(Math.random() * 3) + 1;
   console.log(randomNumber);
 
-  switch(randomNumber) {
+  switch (randomNumber) {
     case 1:
-      return "Rock";
+      return "rock";
       break;
     case 2:
-      return "Paper";
+      return "paper";
       break;
     case 3:
-      return "Scissors";
+      return "scissors";
       break;
+  }
+}
+
+function playRound(playerSelection, computerSelection) {
+  userSelection = userSelection.toLowerCase();
+  
+  switch (userSelection) {
+    case "rock":
+      if (computerSelection === "paper") {
+        return "You Lose! Paper beats Rock!";
+      } else if (computerSelection === "scissors") {
+        return "You Win! Rock beats Scissors!";
+      } else if (computerSelection === "rock") {
+        return "It's a draw!";
+      }
+    case "paper":
+      if (computerSelection === "rock") {
+        return "You Win! Paper beats Rock!";
+      } else if (computerSelection === "scissors") {
+        return "You Lose! Scissors beats Paper!";
+      } else if (computerSelection === "paper") {
+        return "It's a draw!";
+      }
+    case "scissors":
+      if (computerSelection === "rock") {
+        return "You Lose! Rock beats Scissors!";
+      } else if (computerSelection === "paper") {
+        return "You Win! Scissors beats Paper!";
+      } else if (computerSelection === "scissors") {
+        return "It's a draw!";
+      }
+    default:
+      return "Invalid option.";
   }
 }
